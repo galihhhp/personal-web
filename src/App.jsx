ab2d11a4e6f89b84509e7451c11a99835114bd6a
galihhhp <pangestu.galihhhp@gmail.com>
2021-03-18 09:02:11 +0700

    initialize personal web project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Homepage } from "pages";
+import "./styles/index.scss";
+
+function App() {
+  return (
+    <div className="d-flex justify-content-center align-items-center home">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Perkenalkan, Saya Pangestu Galih Pambayun</h1>
+    </div>
+  );
+};
+
+export default Homepage;
